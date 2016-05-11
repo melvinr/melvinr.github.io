664a53d86bba86e0998d3e65e79c317984e883fe
melvinr <melvinr@MBP-van-Melvin.local>
2016-05-11 13:27:03 +0200

    Rewrote HTML, CSS and JS, changed demo

---
+++ Browser Technologies/Week 3-versie 2/js/scriptnew.js
@@ -0,0 +1,85 @@
+var matchup = document.getElementById('matchup'),
+    score = document.getElementById('score'),
+    nextUrl = score.getAttribute('data-to-page');
+
+if (isNewNotificationSupported()) {
+    if (Notification.permission === 'granted') {
+        console.log('jup');
+        newNotification = true;
+
+    } else if (Notification.permission == 'denied') {
+        newNotification = false;
+    } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission(function() {
+            if (Notification.permission === 'granted') {
+                newNotification = true;
+                console.log('I have asked, got accepted');
+            } else if (Notification.permission === 'denied') {
+                newNotification = false;
+                console.log('no support');
+            }
+        });
+    }
+}
+
+
+function isNewNotificationSupported() {
+    if (!window.Notification || !Notification.requestPermission) {
+        return false;
+    }
+    if (Notification.permission == 'granted') {
+        return true;
+    }
+    try {
+        new Notification('...');
+    } catch (e) {
+        if (e.name == 'TypeError')
+            alert('something has gone wrong')
+        return false;
+    }
+
+    return true;
+}
+
+
+function nextPage() {
+  $.ajax({
+    url: nextUrl,
+    type: "GET",
+    success: function(data) {
+      scoreNow = $(data).find('#score');
+      newScore(scoreNow);
+    }
+  });
+}
+
+setTimeout(function() {
+    nextPage();
+}, 5000);
+
+
+function newScore(scoreNow) {
+    nextUrl = scoreNow[0].getAttribute('data-to-page');
+
+    if (nextUrl !== null) {
+        setTimeout(function() {
+            nextPage();
+        }, 5000);
+    }
+    score.innerHTML = scoreNow[0].textContent;
+    scoreNotification(scoreNow[0]);
+}
+
+
+function scoreNotification(scoreNow) {
+    var todayMatchup = matchup.getAttribute('data-match');
+
+    if (newNotification == true) {
+        new Notification(todayMatchup, {
+            icon: 'images/leicester.png',
+            body: scoreNow.innerHTML
+        });
+    } else {
+        console.log('no support');
+    }
+};
